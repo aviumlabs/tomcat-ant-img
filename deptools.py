c7bb01e48402a789e74cdf2e82c8cb7bcdf57006
Michael Konrad <mkonrad@aviumlabs.com>
2026-07-01 10:51:36 +0100

    chore: support additional text interface commands in deptools.py.

--- deptools.py
+++ deptools.py
@@ -72,7 +72,7 @@ def main(argv):
         sys.exit(2)
 
 
-def deploy_war(war_file_path):
+def deploy_war(war_file_path: str):
     logger.info("Preparing deployment...")
     print("Preparing deployment...")
 
@@ -97,7 +97,26 @@ def deploy_war(war_file_path):
         print(f"Web application archive {war_file_path} not found.")
 
 
-def start_app(app):
+def reload_app(app: str):
+    if app:
+        if check_valid_characters(app):
+            logger.info(f"Reloading application {app}...")
+            print(f"Reloading application {app}...")
+            tomcat_url = get_tomcat_mgr_url()
+            tomcat_user, tomcat_pass = get_tomcat_rpa_credentials()
+            start_url = f"{tomcat_url}reload?path=/{app}"
+            response = requests.put(start_url, auth=(tomcat_user, tomcat_pass), data=app, verify=False)
+            if 200 == response.status_code:
+                logger.info(f"Reload response..{response.text}")
+                print(f"Reload response..{response.text}")
+            else:
+                logger.error(f"Failed to reload {app}. HTTP status code..{response.status_code}.")
+                print(f"Failed to reload {app}. HTTP status code..{response.status_code}.")
+                logger.error(f"Reload response..{response.text}")
+                print(f"Reload response..{response.text}.")       
+
+
+def start_app(app: str):
     if app:
         if check_valid_characters(app):
             logger.info(f"Starting application {app}...")
@@ -107,17 +126,47 @@ def start_app(app):
             tomcat_user, tomcat_pass = get_tomcat_rpa_credentials()
             start_url = f"{tomcat_url}start?path=/{app}"
             response = requests.put(start_url, auth=(tomcat_user, tomcat_pass), data=app, verify=False)
-            if response.status_code == 200:
+            if 200 == response.status_code:
+                logger.info(f"Start response..{response.text}.")
+                print(f"Start response..{response.text}.")
                 logger.info(f"{app} started successfully.")
                 print(f"{app} started successfully.")
             else:
-                logger.error(f"Failed to start {app}. HTTP status code: {response.status_code}")
-                print(f"Failed to start {app}. HTTP status code: {response.status_code}")
+                logger.error(f"Failed to start {app}. HTTP status code..{response.status_code}")
+                logger.error(f"Start response...{response.text}.")
+                print(f"Failed to start {app}. HTTP status code..{response.status_code}")
+                print(f"Start response..{response.text}.")
                 sys.exit(1)
         else:
             logger.error("Application name provided is invalid.")
             print("Application name provided is invalid.")
-   
+
+
+def stop_app(app: str):
+    if app:
+        if check_valid_characters(app):
+            logger.info(f"Stopping application {app}...")
+            print(f"Stopping application {app}...")
+
+            tomcat_url = get_tomcat_mgr_url()
+            tomcat_user, tomcat_pass = get_tomcat_rpa_credentials()
+            start_url = f"{tomcat_url}stop?path=/{app}"
+            response = requests.put(start_url, auth=(tomcat_user, tomcat_pass), data=app, verify=False)
+            if 200 == response.status_code:
+                logger.info(f"Stop response...{response.text}.")
+                print(f"Stop response...{response.text}.")
+                logger.info(f"{app} stopped successfully.")
+                print(f"{app} stopped successfully.")
+            else:
+                logger.error(f"Failed to stop {app}. HTTP status code..{response.status_code}")
+                logger.error(f"Stop response...{response.text}.")
+                print(f"Failed to stop {app}. HTTP status code..{response.status_code}")
+                print(f"Stop response...{response.text}.")
+                sys.exit(1)
+        else:
+            logger.error("Application name provided is invalid.")
+            print("Application name provided is invalid.")
+
 
 def backup_db(database: str):
     backup_path = os.environ['BACKUP_HOME']
@@ -182,10 +231,10 @@ def create_database(database: str):
         print("Please specify the database to create.")
 
 
-def check_valid_characters(value):
+def check_valid_characters(value: str):
     if not re.match("^[a-z]*$", value):
         return False
-    
+    # Only alpha characters allowed
     return True
 
 def get_postgresql_url(database: str = 'postgres') -> str:
@@ -224,9 +273,8 @@ def get_tomcat_mgr_url() -> str:
     tls_port = os.environ['TC_SECURE_PORT']
     if not tls_port:
         tls_port = '8443'
-
+    # Tomcat Manager text interface
     tomcat_url = f'https://localhost:{tls_port}/manager/text/'
-
     return tomcat_url
 
 
@@ -238,7 +286,6 @@ def get_tomcat_rpa_credentials():
     if os.path.exists(rpauser_pass_path):
         with open(rpauser_pass_path, 'r') as f:
             rpauser_pass = f.read().strip()
-
         if rpauser_pass and rpa_user:
             return rpa_user, rpauser_pass
         else:
@@ -258,6 +305,31 @@ def install_db_schema(sql_file: str, database: str = 'postgres'):
         logger.error(f"Schema file not found...{sql_file}")
         print(f"Schema file not found... {sql_file}")
         raise FileNotFoundError(f"SQL file not found.")
+    
+
+def is_app_running(app: str) -> bool:
+    # curl -u username:password http://localhost:8080/manager/text/list
+    if app:
+        if check_valid_characters(app):
+            tomcat_url = get_tomcat_mgr_url()
+            tomcat_user, tomcat_pass = get_tomcat_rpa_credentials()
+            start_url = f"{tomcat_url}list"
+            response = requests.get(start_url, auth=(tomcat_user, tomcat_pass), data=app, verify=False)
+            if 200 == response.status_code:
+                results = response.text.splitlines()
+                match_term = f"/{app}:running"
+                for line in results:
+                    if re.match(match_term, line):
+                        app_status = True
+            else:
+                app_status = False
+            return app_status
+        else:
+            logger.error("Invalid application name provided.")
+            return False
+    else:
+        logger.error("Application name is required.")
+        return False
 
 
 def load_sql_file(pg_url, sql_file_path):
@@ -287,17 +359,15 @@ def parse_config():
     conf_file = os.path.join(os.environ['SECRETS_HOME'], 'tomcat.config')
     parser = configparser.ConfigParser()
     parser.read(conf_file)
-
+    # Tomcat configuration dictionary
     tomcat_config = {}
-
     if 'tomcat' in parser:
         tomcat_config = dict(parser['tomcat'])
-
+    # Keystore configuration dictionary
     keystore_config = {}
-
     if 'keystore' in parser:
         keystore_config = dict(parser['keystore'])
-
+    # return configuration dictionaries
     return tomcat_config, keystore_config
 
 
